fb99197e1e98f6ef0174a356db71b16f2f27255f
Mitzi Estrada <m_estrada17@live.com>
2020-02-23 18:41:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall][:columbus_day] = "Flags", "Parade Floats", "Italian Food"
-  holiday_hash[:winter][:valentines_day] = "Cupid Cut-Out", "Candy Hearts"
+  holiday_hash[:fall][:columbus_day] = supply_array
+  holiday_hash[:winter][:valentines_day] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
